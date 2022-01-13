8b155acb107c0e99768876583be652f889c9ad39
schen <schen@DESKTOP-D15UOBP>
2022-01-12 21:42:21 -0500

    Made exception serializable

--- PostgresQueryCreator/PQCException.cs
+++ PostgresQueryCreator/PQCException.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace PostgresQueryCreator
 {
+    [Serializable]
     public class PqcException : Exception
     {
+        public PqcException() { }
         public PqcException(string message, Exception innerException)
        : base(message, innerException) { }
 
         public PqcException(string message)
     : base(message) { }
+     protected internal PqcException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
     }
 }
